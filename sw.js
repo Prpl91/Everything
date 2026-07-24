d3958ad915b05c513e6647386b784d9de59ad34a
Prpl91 <firpomaryan@gmail.com>
2026-07-24 18:37:18 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const C="ciclo-v1";
+self.addEventListener("install",e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./move.html","./eat.html","./icon-192.png","./icon-512.png","./logo.png"])));self.skipWaiting();});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))));});
+self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const cl=res.clone();caches.open(C).then(c=>c.put(e.request,cl));return res;}).catch(()=>caches.match("./index.html"))));});
